78622cd0fd19bb5da083c5a5f70606f2e56a1ad3
Sebastian Benkö <Seki@Sebastians-MacBook-Pro.local>
2018-10-05 18:48:23 +0200

    .

--- myproject/venv/test.py
+++ myproject/venv/test.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+@app.route("/<string:name>")
+def hello_world(name):
+    name = name.capitalize()
+    return f"<h1>Hello, {name}</h1>"
